8a3689c30bb6d9cc878fddf8dc86901962248c18
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 01:48:53 +0200

    AutoSlate: Formulaire contact

---
+++ components/ContactMap.tsx
@@ -0,0 +1,7 @@
+export default function ContactMap() {
+  return (
+    <div className="w-full h-64 bg-gray-200 rounded-lg flex items-center justify-center">
+      <p>Carte localisant BoiteMini</p>
+    </div>
+  );
+}
