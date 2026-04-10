3b3b3ef0447af344cc1dcbde9b746e3a099f8c18
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 00:25:08 +0200

    AutoSlate: Design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,16 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        slate: {
-          50: '#f8fafc',
-          900: '#0f172a',
-          800: '#1e293b',
-        },
-        amber: {
-          500: '#d97706',
-        },
+        slate: { 50: '#f8fafc', 400: '#94a3b8', 700: '#334155', 800: '#1e293b', 900: '#0f172a' },
+        amber: { 500: '#d97706' },
       },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+      container: {
+        center: true,
+        padding: '1.5rem',
       },
     },
   },
