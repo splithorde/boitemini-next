842ed7c8206b18c6122ac005679bc8f0ff25076b
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 01:37:20 +0200

    AutoSlate: Formulaire contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,14 @@
-import ContactForm from '@/components/contact/ContactForm';
+import ContactInfo from '@/components/ContactInfo';
+import ContactForm from '@/components/ContactForm';
 
 export default function Contact() {
   return (
-    <div className="container mx-auto py-16">
-      <h1 className="text-4xl font-bold text-center mb-12">Nous Contacter</h1>
-      <ContactForm />
+    <div className="container mx-auto px-6 py-16">
+      <h1 className="text-4xl font-bold text-blue-600 mb-12 text-center">Nous Contacter</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <ContactInfo />
+        <ContactForm />
+      </div>
     </div>
   );
 }
