b633cd54f0206f8a6939794dde8abdb22b7652de
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-10 22:42:14 +0200

    AutoSlate: Cleanup redundant file app/components/Header.tsx

--- app/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-blue-700 text-white shadow-md">
-      <div className="container mx-auto px-4 py-6">
-        <h1 className="text-3xl font-bold tracking-tight">BoiteMini</h1>
-      </div>
-    </header>
-  );
-}
