37f34d074885735d366fcee84599b5b47ea303e7
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 01:39:41 +0200

    AutoSlate: Formulaire contact

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: parseInt(process.env.SMTP_PORT || '465'),
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
