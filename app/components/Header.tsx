960b22f786d19459647b747ab8e22bf5a120256f
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-10 22:27:59 +0200

    AutoSlate: Structure site

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-700 text-white shadow-md">
+      <div className="container mx-auto px-4 py-6">
+        <h1 className="text-3xl font-bold tracking-tight">BoiteMini</h1>
+      </div>
+    </header>
+  );
+}
