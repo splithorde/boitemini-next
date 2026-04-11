ebdc270ddf8a86305e31ccc75de0cd2b262d27c3
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 09:57:00 +0200

    AutoSlate: Formulaire contact

--- components/ContactMap.tsx
+++ components/ContactMap.tsx
@@ -1,15 +1,17 @@
 export default function ContactMap() {
+  const address = "25bis Rue Georges Bizet, 92000 Nanterre, France";
   return (
-    <div className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden relative">
+    <div className="w-full h-80 rounded-xl overflow-hidden shadow-md mt-8">
       <iframe
         width="100%"
         height="100%"
-        frameBorder="0"
-        title="map"
-        marginHeight={0}
-        marginWidth={0}
-        scrolling="no"
-        src="https://maps.google.com/maps?width=100%&height=600&hl=fr&q=25bis%20Rue%20Georges%20Bizet%20Nanterre&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+        style={{ border: 0 }}
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/place?key=YOUR_GOOGLE_MAPS_API_KEY&q=${encodeURIComponent(address)}`}
+        // Alternative non-API key embed for display
+        srcDoc={`<style>html,body{margin:0;height:100%}</style><iframe width="100%" height="100%" frameborder="0" src="https://maps.google.com/maps?q=${encodeURIComponent(address)}&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe>`}
       ></iframe>
     </div>
   );
