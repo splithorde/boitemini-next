4a73cc8bba18e523b80a8ff1069ae006ab03c6a0
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 02:22:12 +0200

    AutoSlate: Formulaire contact

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,13 +1,11 @@
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || "smtp.orange.fr",
-  port: parseInt(process.env.SMTP_PORT || "465"),
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST || 'smtp.orange.fr',
+  port: parseInt(process.env.SMTP_PORT || '465'),
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
-
-export default transporter;
