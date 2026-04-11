30606538b261d87ba00f055710a1cf981e674c9f
splithorde <97248887+splithorde@users.noreply.github.com>
2026-04-11 09:47:49 +0200

    AutoSlate: Formulaire contact

--- components/ContactMap.tsx
+++ components/ContactMap.tsx
@@ -1,9 +1,16 @@
 export default function ContactMap() {
   return (
     <div className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden relative">
-      <div className="absolute inset-0 flex items-center justify-center text-gray-500">
-        <span className="text-center px-4">Carte interactive (Simulée)<br/>25bis Rue Georges Bizet, Nanterre</span>
-      </div>
+      <iframe
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        title="map"
+        marginHeight={0}
+        marginWidth={0}
+        scrolling="no"
+        src="https://maps.google.com/maps?width=100%&height=600&hl=fr&q=25bis%20Rue%20Georges%20Bizet%20Nanterre&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+      ></iframe>
     </div>
   );
 }
